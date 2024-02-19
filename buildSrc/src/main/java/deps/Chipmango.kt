2dcecb43ad567cdd25bdc14ce753e2c54fb89d77
Dzung <android02@solidwaretech.com>
2024-02-19 17:49:45 +0700

    Update latest chipmango

--- buildSrc/src/main/java/deps/Chipmango.kt
+++ buildSrc/src/main/java/deps/Chipmango.kt
@@ -1,7 +1,7 @@
 package deps
 
 object Chipmango : Dependency() {
-    private const val version = "0.3.3"
+    private const val version = "0.3.6"
     private const val chipmangoCore = "io.github.tiendung717:chipmango:$version"
 
     override fun implementations() = listOf<String>(
